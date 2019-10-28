331bdfb2edbfba0d9dae200ffa5871bdf9abf01f
yuki takahashi <docomo.ne.jp.gs3@gmail.com>
2019-10-28 07:07:56 +0000

    timline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "メッセージを投稿しました。"
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc).page(parmas[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(parmas[:page])
       flash.now[:danger] = "メッセージの投稿に失敗しました。"
       render "toppages/index"
     end
